3ec76a07d2e33803eed45c0ade00e876f2d8a113
Catie <130665030+Cat-Ling@users.noreply.github.com>
2024-01-23 18:18:19 +0530

    add config options
    one more thing contributing to ease of use

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    // Config for all the functions somebody might want to tinker with.
+
+// Enable it if you want your console to go woooooosh!!!.
+debuggerOption: false,
+
+// Proxy Config / set `false` to disable proxies
+proxyEnable: true,
+
+// DNS Config / set to `false` if you want the app to use system dns.
+SecureDnsEnable: true,
+
+// AdBlock Config / you know the jazz
+AdBlockEnable: true
+}
