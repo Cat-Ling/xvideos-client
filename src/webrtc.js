ad462f841b157517e72f8416dc62272b3c554cc0
Catie <130665030+Cat-Ling@users.noreply.github.com>
2024-02-08 15:20:55 +0530

    Update webrtc.js

--- src/webrtc.js
+++ src/webrtc.js
@@ -1,3 +1,5 @@
+// I know we don't need this in a separate file. But I'll do it anyway. Fight me.
+
 function setWebRTCBlockingPolicy(webContents) {
     webContents.setWebRTCIPHandlingPolicy('disable_non_proxied_udp');
   }
