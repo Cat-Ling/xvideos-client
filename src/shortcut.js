25e5acbecb12d4dd9e6fba83a798d389e5380a98
Catie <130665030+Cat-Ling@users.noreply.github.com>
2024-02-08 11:31:42 +0530

    Update shortcut.js
    removed some code to prevent memory leak warnings when ctrl+s was spammed. I'll add it back when I fix it.

--- src/shortcut.js
+++ src/shortcut.js
@@ -9,6 +9,7 @@
 const { app, globalShortcut, BrowserWindow } = require('electron');
 const { GetProxy, ConnectProxy } = require('./proxy');
 let urlbarWindow;
+let shortcutsRegistered = false;
 
 function createurlbarWindow(mainWindow) {
   urlbarWindow = new BrowserWindow({
@@ -30,163 +31,167 @@ function createurlbarWindow(mainWindow) {
   urlbarWindow.loadFile('public/urlbar.html');
 }
 
-function registerShortcuts(mainWindow) {
-  const isMainWindowFocused = () => mainWindow && mainWindow.isFocused();
-  const isUrlbarWindowFocused = () => urlbarWindow && urlbarWindow.isFocused();
-  const isUrlbarWindowOpen = () => !!urlbarWindow;
-
-  // Function to unregister shortcuts when the application loses focus
-  const unregisterShortcutsOnBlur = () => {
-    globalShortcut.unregister('Control+R');
-    globalShortcut.unregister('Control+Shift+R');
-    globalShortcut.unregister('Control+Space');
-    globalShortcut.unregister('Control+S');
-    globalShortcut.unregister('Control+W');
-    globalShortcut.unregister('Control+Q');
-    globalShortcut.unregister('F5');
-    globalShortcut.unregister('F1');
-    globalShortcut.unregister('Escape');
-    globalShortcut.unregister('Control+F5');
-    globalShortcut.unregister('Control+Left');
-    globalShortcut.unregister('Control+Right');
-  };
-
-  globalShortcut.register('Control+R', () => {
-    if (isMainWindowFocused()) {
-      mainWindow.reload();
-    } else if (isUrlbarWindowFocused()) {
-      urlbarWindow.reload();
-    }
-  });
-
-  globalShortcut.register('Control+Shift+R', () => {
-    if (isMainWindowFocused()) {
-      mainWindow.webContents.reloadIgnoringCache();
-    } else if (isUrlbarWindowFocused()) {
-      urlbarWindow.webContents.reloadIgnoringCache();
-    }
-  });
-
-  // Had to add this one since I was tired of reopening the program when a proxy didn't work.
-  const executeWithDelay = async (callback, delay) => {
-    await new Promise((resolve) => setTimeout(resolve, delay));
-    callback();
-  };
-  
-  globalShortcut.register('Control+Space', async () => {
-    if (isMainWindowFocused()) {
-      GetProxy();
-  
-      await executeWithDelay(() => {
-        ConnectProxy(mainWindow);
-      }, 1000);
-  
-  
-      await executeWithDelay(() => {
-        mainWindow.webContents.reload();
-      }, 2000);
-  
-    } else if (isUrlbarWindowFocused()) {
-      GetProxy();
-  
-      await executeWithDelay(() => {
-        ConnectProxy(mainWindow);
-      }, 1000);
-  
-  
-      await executeWithDelay(() => {
-        urlbarWindow.webContents.reload();
-      }, 2000);
-    }
-  });
-  
-
-
-  globalShortcut.register('Control+S', () => {
-    if (isMainWindowFocused()) {
-      if (!isUrlbarWindowOpen()) {
-        createurlbarWindow(mainWindow);
-      }
-    } else if (isUrlbarWindowFocused()) {
-      urlbarWindow.webContents.loadFile('public/urlbar.html');
-    }
-  });
-
-  globalShortcut.register('Control+W', () => {
-    if (isMainWindowFocused()) {
-      mainWindow.close();
-    } else if (isUrlbarWindowFocused()) {
-      urlbarWindow.close();
-    }
-  });
-
-  globalShortcut.register('Control+Q', () => {
-    app.quit();
-  });
-
-  globalShortcut.register('F5', () => {
-    if (isMainWindowFocused()) {
-      mainWindow.webContents.reload();
-    } else if (isUrlbarWindowFocused()) {
-      urlbarWindow.webContents.reload();
-    }
-  });
-
-  globalShortcut.register('F1', () => {
-    if (isMainWindowFocused()) {
-      mainWindow.loadURL('https://github.com/Cat-Ling/xvideos-client?tab=readme-ov-file#keyboard-shortcuts');
-    } else if (isUrlbarWindowFocused()) {
-      urlbarWindow.loadURL('https://github.com/Cat-Ling/xvideos-client?tab=readme-ov-file#keyboard-shortcuts');
-    }
-  });
-
-  globalShortcut.register('Escape', () => {
-      app.quit();
-
-  });
-
-  globalShortcut.register('Control+F5', () => {
-    if (isMainWindowFocused()) {
-      mainWindow.webContents.reloadIgnoringCache();
-    } else if (isUrlbarWindowFocused()) {
-      urlbarWindow.webContents.reloadIgnoringCache();
-    }
-  });
-
-
-  globalShortcut.register('Control+Left', () => {
-    if (isMainWindowFocused()) {
-      if (mainWindow.webContents.canGoBack()) {
-        mainWindow.webContents.goBack();
-      }
-    } else if (isUrlbarWindowFocused()) {
-      if (urlbarWindow.webContents.canGoBack()) {
-        urlbarWindow.webContents.goBack();
-      }
-    }
-  });
-
-  globalShortcut.register('Control+Right', () => {
-    if (isMainWindowFocused()) {
-      if (mainWindow.webContents.canGoForward()) {
-        mainWindow.webContents.goForward();
-      }
-    } else if (isUrlbarWindowFocused()) {
-      if (urlbarWindow.webContents.canGoForward()) {
-        urlbarWindow.webContents.goForward();
-      }
-    }
-  });
-
-  app.on('browser-window-blur', () => {
-    unregisterShortcutsOnBlur();
-  });
-  app.on('browser-window-focus', () => {
-    registerShortcuts(mainWindow);
-  });
-  app.on('will-quit', () => {
-    globalShortcut.unregisterAll();
-  });
+function unregisterShortcuts() {
+  globalShortcut.unregister('Control+R');
+  globalShortcut.unregister('Control+Shift+R');
+  globalShortcut.unregister('Control+Space');
+  globalShortcut.unregister('Control+S');
+  globalShortcut.unregister('Control+W');
+  globalShortcut.unregister('Control+Q');
+  globalShortcut.unregister('F5');
+  globalShortcut.unregister('F1');
+  globalShortcut.unregister('Escape');
+  globalShortcut.unregister('Control+F5');
+  globalShortcut.unregister('Control+Left');
+  globalShortcut.unregister('Control+Right');
+  shortcutsRegistered = false;
 }
+
+function registerShortcuts(mainWindow) {
+  if (!shortcutsRegistered) {
+    const isMainWindowFocused = () => mainWindow && mainWindow.isFocused();
+    const isUrlbarWindowFocused = () => urlbarWindow && urlbarWindow.isFocused();
+    const isUrlbarWindowOpen = () => !!urlbarWindow;
+
+    globalShortcut.register('Control+R', () => {
+      if (isMainWindowFocused()) {
+        mainWindow.reload();
+      } else if (isUrlbarWindowFocused()) {
+        urlbarWindow.reload();
+      }
+    });
+
+    globalShortcut.register('Control+Shift+R', () => {
+      if (isMainWindowFocused()) {
+        mainWindow.webContents.reloadIgnoringCache();
+      } else if (isUrlbarWindowFocused()) {
+        urlbarWindow.webContents.reloadIgnoringCache();
+      }
+    });
+
+    const executeWithDelay = async (callback, delay) => {
+      await new Promise((resolve) => setTimeout(resolve, delay));
+      callback();
+    };
+    
+    globalShortcut.register('Control+Space', async () => {
+      if (isMainWindowFocused()) {
+        GetProxy();
+    
+        await executeWithDelay(() => {
+          ConnectProxy(mainWindow);
+        }, 1000);
+    
+    
+        await executeWithDelay(() => {
+          mainWindow.webContents.reload();
+        }, 2000);
+    
+      } else if (isUrlbarWindowFocused()) {
+        GetProxy();
+    
+        await executeWithDelay(() => {
+          ConnectProxy(mainWindow);
+        }, 1000);
+    
+    
+        await executeWithDelay(() => {
+          urlbarWindow.webContents.reload();
+        }, 2000);
+      }
+    });
+
+    globalShortcut.register('Control+S', () => {
+      if (isMainWindowFocused()) {
+        if (!isUrlbarWindowOpen()) {
+          createurlbarWindow(mainWindow);
+        }
+      } else if (isUrlbarWindowFocused()) {
+        urlbarWindow.webContents.loadFile('public/urlbar.html');
+      }
+    });
+
+    globalShortcut.register('Control+W', () => {
+      if (isMainWindowFocused()) {
+        mainWindow.close();
+      } else if (isUrlbarWindowFocused()) {
+        urlbarWindow.close();
+      }
+    });
+
+    globalShortcut.register('Control+Q', () => {
+      app.quit();
+    });
+
+    globalShortcut.register('F5', () => {
+      if (isMainWindowFocused()) {
+        mainWindow.webContents.reload();
+      } else if (isUrlbarWindowFocused()) {
+        urlbarWindow.webContents.reload();
+      }
+    });
+
+    globalShortcut.register('F1', () => {
+      if (isMainWindowFocused()) {
+        mainWindow.loadURL('https://github.com/Cat-Ling/xvideos-client?tab=readme-ov-file#keyboard-shortcuts');
+      } else if (isUrlbarWindowFocused()) {
+        urlbarWindow.loadURL('https://github.com/Cat-Ling/xvideos-client?tab=readme-ov-file#keyboard-shortcuts');
+      }
+    });
+
+    globalShortcut.register('Escape', () => {
+      app.quit();
+    });
+
+    globalShortcut.register('Control+F5', () => {
+      if (isMainWindowFocused()) {
+        mainWindow.webContents.reloadIgnoringCache();
+      } else if (isUrlbarWindowFocused()) {
+        urlbarWindow.webContents.reloadIgnoringCache();
+      }
+    });
+
+    globalShortcut.register('Control+Left', () => {
+      if (isMainWindowFocused()) {
+        if (mainWindow.webContents.canGoBack()) {
+          mainWindow.webContents.goBack();
+        }
+      } else if (isUrlbarWindowFocused()) {
+        if (urlbarWindow.webContents.canGoBack()) {
+          urlbarWindow.webContents.goBack();
+        }
+      }
+    });
+
+    globalShortcut.register('Control+Right', () => {
+      if (isMainWindowFocused()) {
+        if (mainWindow.webContents.canGoForward()) {
+          mainWindow.webContents.goForward();
+        }
+      } else if (isUrlbarWindowFocused()) {
+        if (urlbarWindow.webContents.canGoForward()) {
+          urlbarWindow.webContents.goForward();
+        }
+      }
+    });
+
+    shortcutsRegistered = true;
+  }
+}
+
+app.on('browser-window-blur', () => {
+  unregisterShortcuts();
+});
+
+app.on('browser-window-focus', () => {
+  const mainWindow = BrowserWindow.getFocusedWindow();
+  if (mainWindow) {
+    registerShortcuts(mainWindow);
+  }
+});
+
+app.on('will-quit', () => {
+  globalShortcut.unregisterAll();
+});
 
 module.exports = registerShortcuts;
